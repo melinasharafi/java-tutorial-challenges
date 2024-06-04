0648e4a4092e261e9f6898921fb5c8d93c85c7a5
melinasharafi <melinasharafimiab@gmail.com>
2024-06-04 13:56:29 +0330

    a method for counting (1) in a binary number created.

--- src/codeware/NextHigherInt.java
+++ src/codeware/NextHigherInt.java
@@ -1,4 +1,23 @@
 package codeware;
 
+import java.util.ArrayList;
+
 public class NextHigherInt {
+
+// Your task is to find the next higher number (int) with same '1'- Bits.
+
+    public static int nextHigher(int n) {
+        String nInBinary = Integer.toBinaryString(n);
+
+        return 0;
+    }
+    public static int countOne (String intInBinary) {
+        int count = 0;
+        String[] parsedString = intInBinary.split("");
+        for (int i = 0;i < parsedString.length; i++) {
+            if (parsedString[i].equals("1"))
+                count++;
+        }
+        return count;
+    }
 }
