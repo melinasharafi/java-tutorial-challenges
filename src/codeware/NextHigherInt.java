b1af25aab5827e9d940daf9ffa379e2f5268b784
melinasharafi <melinasharafimiab@gmail.com>
2024-06-09 21:22:20 +0330

    change countOne() to accept integar.

--- src/codeware/NextHigherInt.java
+++ src/codeware/NextHigherInt.java
@@ -1,29 +1,38 @@
 package codeware;
 
-import java.util.ArrayList;
-
 public class NextHigherInt {
 
 // Your task is to find the next higher number (int) with same '1'- Bits.
+// https://www.codewars.com/kata/56bdd0aec5dc03d7780010a5/train/java
 
-    public static int nextHigher(int n) {
+//    public static int nextHigher(int n) {
+//        String nInBinary = Integer.toBinaryString(n);
+//        int nextHigher = n + 1;
+//        String nextHigherInBinary = Integer.toBinaryString(nextHigher);
+//        boolean check = true;
+//        while (check) {
+//            if (countOne(nInBinary) == countOne(nextHigherInBinary)) {
+//                break;
+//            }
+//            nextHigher++;
+//
+//        }
+//        return nextHigher;
+//    }
+
+    public static int countOne(int n) {
         String nInBinary = Integer.toBinaryString(n);
-        int nextHigher = n;
-        String nextHigherInBinary = Integer.toBinaryString(nextHigher);
-        boolean check = ( countOne(nInBinary) == countOne(nextHigherInBinary) );
-        while (!check) {
-            nextHigher++;
-        }
-
-        return nextHigher;
-    }
-    public static int countOne (String intInBinary) {
         int count = 0;
-        String[] parsedString = intInBinary.split("");
-        for (int i = 0;i < parsedString.length; i++) {
-            if (parsedString[i].equals("1"))
+        String[] parsedString = nInBinary.split("");
+        for (String s : parsedString) {
+            if (s.equals("1"))
                 count++;
         }
         return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countOne(127));
+        System.out.println(countOne(128));
     }
 }
