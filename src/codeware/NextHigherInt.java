3406a000256a7f68185c3acfbcd0cc962f39947a
melinasharafi <melinasharafimiab@gmail.com>
2024-06-09 21:56:52 +0330

    optimize countOne() mthod.

--- src/codeware/NextHigherInt.java
+++ src/codeware/NextHigherInt.java
@@ -2,37 +2,44 @@ package codeware;
 
 public class NextHigherInt {
 
-// Your task is to find the next higher number (int) with same '1'- Bits.
+    // Your task is to find the next higher number (int) with same '1'- Bits.
 // https://www.codewars.com/kata/56bdd0aec5dc03d7780010a5/train/java
+    public static int nextHigher(int n) {
+        int nextHigherInt = n + 1;
 
-//    public static int nextHigher(int n) {
-//        String nInBinary = Integer.toBinaryString(n);
-//        int nextHigher = n + 1;
-//        String nextHigherInBinary = Integer.toBinaryString(nextHigher);
-//        boolean check = true;
-//        while (check) {
-//            if (countOne(nInBinary) == countOne(nextHigherInBinary)) {
-//                break;
-//            }
-//            nextHigher++;
-//
-//        }
-//        return nextHigher;
-//    }
+        while (!(countOne(n) == countOne(nextHigherInt))) {
+            nextHigherInt++;
+        }
+
+        return nextHigherInt;
+    }
 
     public static int countOne(int n) {
-        String nInBinary = Integer.toBinaryString(n);
         int count = 0;
-        String[] parsedString = nInBinary.split("");
-        for (String s : parsedString) {
-            if (s.equals("1"))
-                count++;
+        while (n != 0) {
+            count++;
+            n &= (n - 1);
         }
         return count;
     }
 
+//    public static int countOne(int n) {
+//        String nInBinary = Integer.toBinaryString(n);
+//        String[] parsedString = nInBinary.split("");
+//
+//        int count = 0;
+//
+//        for (String s : parsedString) {
+//            if (s.equals("1"))
+//                count++;
+//        }
+//        return count;
+//    }
+
     public static void main(String[] args) {
         System.out.println(countOne(127));
         System.out.println(countOne(128));
+        System.out.println(nextHigher(201326592));
+        
     }
 }
