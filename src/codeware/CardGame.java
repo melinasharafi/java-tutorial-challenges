faa21773c54dfcf428a5d41fad0473325b6cf357
melinasharafi <melinasharafimiab@gmail.com>
2024-07-08 20:33:27 +0330

    code description added.

--- src/codeware/CardGame.java
+++ src/codeware/CardGame.java
@@ -3,7 +3,7 @@ package codeware;
 public class CardGame {
     
     /*
-
+    
 Steve and Josh are bored and want to play something. They don't want to think too much, so they come up with a really simple game. Write a function called winner and figure out who is going to win.
 
 They are dealt the same number of cards. They both flip the card on the top of their deck. Whoever has a card with higher value wins the round and gets one point (if the cards are of the same value, neither of them gets a point). After this, the two cards are discarded and they flip another card from the top of their deck. They do this until they have no cards left.
