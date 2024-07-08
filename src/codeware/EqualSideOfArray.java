50ce32f60eb5ad318ca221c7e1778226fa91191b
melinasharafi <melinasharafimiab@gmail.com>
2024-07-08 19:01:18 +0330

    bug fixes.

--- src/codeware/EqualSideOfArray.java
+++ src/codeware/EqualSideOfArray.java
@@ -1,33 +1,34 @@
 package codeware;
-
 public class EqualSideOfArray {
+
     /*
     You are going to be given an array of integers. Your job is to take that array and find an index N where the sum of
      the integers to the left of N is equal to the sum of the integers to the right of N.
     If there is no index that would make this happen, return -1.
      */
-
     public static int findEvenIndex(int[] arr) {
-        // your code
-        int sumOfLeftSide = 0;
-        int sumOfRightSide = 0;
+
         for (int i = 0; i < arr.length; i++) {
 
-            //find the sum of integers to the left side
-            for (int j = 0; j < i; j++) {
-                sumOfLeftSide += arr[j];
+            // hole the summation for each element of arr
+            int sumOfRightSide = 0;
+            int sumOfLeftSide = 0;
+            // summation of right side
+            for (int j = (i + 1); j < arr.length; j++) {
+                sumOfRightSide = sumOfRightSide + arr[j];
             }
 
-            //find the sum of integers to the right side
-            for (int k = i + 1; k < arr.length; k++) {
-                sumOfRightSide += arr[k];
+            // summation of left side
+            for (int k = 0; k < i; k++) {
+                sumOfLeftSide = sumOfLeftSide + arr[k];
             }
 
+            // check if the sumOfRightSide and sumOfLeftSide are equal or not
             if (sumOfLeftSide == sumOfRightSide) {
                 return i;
-
             }
         }
+
         return -1;
     }
 }
