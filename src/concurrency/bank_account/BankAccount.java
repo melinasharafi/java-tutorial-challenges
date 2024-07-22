b538c10ab4f59938ad3cde676baf668736470819
melinasharafi <melinasharafimiab@gmail.com>
2024-07-22 17:59:32 +0330

    use try-finally block to ensure that lock is going to unlocked.

--- src/concurrency/bank_account/BankAccount.java
+++ src/concurrency/bank_account/BankAccount.java
@@ -1,28 +1,35 @@
 package concurrency.bank_account;
 
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class BankAccount {
     private double balance;
     private String accountNumber;
-    private ReentrantLock lock;
+    private Lock lock;
 
     public BankAccount(String accountNumber, double initialBalance) {
         this.accountNumber = accountNumber;
         this.balance = initialBalance;
         this.lock = new ReentrantLock();
-    }
+    } 
 
     public void deposit(double amount) {
         lock.lock();
-        balance += amount;
-        lock.unlock();
+        try {
+            this.balance += amount;
+        } finally {
+            lock.unlock();
+        }
     }
 
     public synchronized void withdraw(double amount) {
         lock.lock();
-        balance -= amount;
-        lock.unlock();
+        try {
+            this.balance -= amount;
+        } finally {
+            lock.unlock();
+        }
     }
 
     public String getAccountNumber() {
