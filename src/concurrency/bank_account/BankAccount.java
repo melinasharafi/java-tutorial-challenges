e850d287aadd04650e07fd38dc4651f15d233f98
melinasharafi <melinasharafimiab@gmail.com>
2024-07-22 17:53:15 +0330

    use re-entrantLock to make a thread safe class

--- src/concurrency/bank_account/BankAccount.java
+++ src/concurrency/bank_account/BankAccount.java
@@ -1,20 +1,28 @@
 package concurrency.bank_account;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class BankAccount {
     private double balance;
     private String accountNumber;
+    private ReentrantLock lock;
 
     public BankAccount(String accountNumber, double initialBalance) {
         this.accountNumber = accountNumber;
         this.balance = initialBalance;
+        this.lock = new ReentrantLock();
     }
 
-    public synchronized void deposit(double amount) {
+    public void deposit(double amount) {
+        lock.lock();
         balance += amount;
+        lock.unlock();
     }
 
     public synchronized void withdraw(double amount) {
+        lock.lock();
         balance -= amount;
+        lock.unlock();
     }
 
     public String getAccountNumber() {
