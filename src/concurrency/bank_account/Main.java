8a4ce7849df1bb7ce94eb2dde2522da7a3bd0c4d
melinasharafi <melinasharafimiab@gmail.com>
2024-07-22 12:26:08 +0330

    code to creat and start thread.

--- src/concurrency/bank_account/Main.java
+++ src/concurrency/bank_account/Main.java
@@ -1,4 +1,24 @@
 package concurrency.bank_account;
 
 public class Main {
+    public static void main(String[] args) {
+
+        BankAccount bankAccount = new BankAccount("12345-678", 1000.00);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                bankAccount.deposit(300.00);
+                bankAccount.withdraw(50.00);
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                bankAccount.deposit(203.75);
+                bankAccount.withdraw(100.00);
+            }
+        }).start();
+    }
 }
