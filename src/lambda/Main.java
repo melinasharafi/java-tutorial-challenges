0269352e98034f2913fdbf5133ec5c8862e382d3
melinasharafi <melinasharafimiab@gmail.com>
2024-07-27 21:24:25 +0330

    implement lambda expression to sort and uppercased a list.

--- src/lambda/Main.java
+++ src/lambda/Main.java
@@ -1,5 +1,9 @@
 package lambda;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
@@ -39,10 +43,41 @@ public class Main {
             return "I love Java";
         };
         System.out.println(supplier.get());
+
+
+        // challenge 9
+        List<String> topNames2015 = Arrays.asList(
+                "Amelia",
+                "Olivia",
+                "emily",
+                "Isla",
+                "Ava",
+                "oliver",
+                "Jack",
+                "Charlie",
+                "harry",
+                "Jacob"
+        );
+
+        Function<List<String>, List<String>> sortedUpperCased = names -> {
+            List<String> upperCased = new ArrayList<>();
+            for (String name : names) {
+                upperCased.add(name.substring(0, 1).toUpperCase() + name.substring(1));
+            }
+            upperCased.sort((s1, s2) -> {
+                return s1.compareTo(s2);
+            });
+            return upperCased;
+        };
+
+        List<String> result = sortedUpperCased.apply(topNames2015);
+        result.forEach(name -> System.out.println(name));
+
     }
 
     // challenge 4
-    private  static void everySecondCharacter(Function function) {
+    private static void everySecondCharacter(Function function) {
         System.out.println(function.apply("1234567890"));
     }
+
 }
