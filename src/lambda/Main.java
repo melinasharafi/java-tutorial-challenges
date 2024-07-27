62dd8acb62fe1400e7dacb26eed4d67f027ee247
melinasharafi <melinasharafimiab@gmail.com>
2024-07-27 20:07:05 +0330

    write a given methos as lambda expression.

--- src/lambda/Main.java
+++ src/lambda/Main.java
@@ -1,8 +1,11 @@
 package lambda;
 
+import java.util.function.Function;
+
 public class Main {
     public static void main(String[] args) {
 
+        // challenge 1
         Runnable runnable = () -> {
             String myString = "Let's split this up into an array";
             String[] parts = myString.split(" ");
@@ -10,6 +13,20 @@ public class Main {
                 System.out.println(part);
             }
         };
+
+
+        // challenge 2
+        Function<String, String> s = (String source) -> {
+            StringBuilder returnVal = new StringBuilder();
+            for (int i = 0; i < source.length(); i++) {
+                if (i % 2 == 1) {
+                    returnVal.append(source.charAt(i));
+                }
+            }
+            return returnVal.toString();
+        };
+        System.out.println(s.apply("melina"));
+
         
     }
 }
