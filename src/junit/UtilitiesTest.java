38f71ed24c813cf5c91647c251e0ebf1e1c37e0f
melinasharafi <melinasharafimiab@gmail.com>
2024-07-31 17:36:56 +0330

    remove repetitive code

--- src/junit/UtilitiesTest.java
+++ src/junit/UtilitiesTest.java
@@ -1,20 +1,27 @@
 
 package junit;
 
+import org.junit.Before;
+
 import static org.junit.Assert.*;
 
 public class UtilitiesTest {
+    Utilities util;
+
+    @Before
+    public void before() {
+        util = new Utilities();
+        System.out.println("Running a test ...");
+    }
 
     @org.junit.Test
     public void everyNthChar() {
-        Utilities util = new Utilities();
         assertArrayEquals(new char[]{'e', 'l'}, util.everyNthChar(new char[]{'h', 'e', 'l', 'l', 'o'}, 2));
         assertArrayEquals(new char[]{'m', 'i'}, util.everyNthChar(new char[]{'m', 'i'}, 4));
     }
 
     @org.junit.Test
     public void removePairs() {
-        Utilities util = new Utilities();
         assertEquals("ABCDEF", util.removePairs("AABCDDEFF"));
         assertEquals("ABCABDEF", util.removePairs("ABCCABDEEF"));
         assertEquals("A", util.removePairs("A"));
@@ -22,20 +29,17 @@ public class UtilitiesTest {
     }
 
     @org.junit.Test
-    public void converter() throws Exception{
-        Utilities util = new Utilities();
+    public void converter() throws Exception {
         assertEquals(300, util.converter(10, 5));
     }
 
     @org.junit.Test(expected = ArithmeticException.class)
     public void converter_ArithmeticException() throws Exception {
-        Utilities util = new Utilities();
         assertEquals(new ArithmeticException(), util.converter(10, 0));
     }
 
     @org.junit.Test
     public void nullIfOddLength() {
-        Utilities util = new Utilities();
         assertEquals("melina", util.nullIfOddLength("melina"));
         assertNull("The String is null", util.nullIfOddLength("melin"));
     }
