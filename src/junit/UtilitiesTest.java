537e0ceb39a6af2a3eb2e7c26ba1a3df8c27f879
melinasharafi <melinasharafimiab@gmail.com>
2024-07-31 16:02:33 +0330

    implement test cases.

---
+++ src/junit/UtilitiesTest.java
@@ -0,0 +1,26 @@
+package junit;
+
+import static org.junit.Assert.*;
+
+public class UtilitiesTest {
+
+    @org.junit.Test
+    public void everyNthChar() {
+        fail("Test code doesn't implemented yet...");
+    }
+
+    @org.junit.Test
+    public void removePairs() {
+        fail("Test code doesn't implemented yet...");
+    }
+
+    @org.junit.Test
+    public void converter() {
+        fail("Test code doesn't implemented yet...");
+    }
+
+    @org.junit.Test
+    public void nullIfOddLength() {
+        fail("Test code doesn't implemented yet...");
+    }
+}
