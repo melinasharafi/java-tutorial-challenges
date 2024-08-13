07cc3b007cce49d84023459c89177b0170a458de
melinasharafi <melinasharafimiab@gmail.com>
2024-08-13 19:45:28 +0330

    implement a method to break up camel casing

--- src/codeware/BreakCamelCase.java
+++ src/codeware/BreakCamelCase.java
@@ -1,4 +1,25 @@
 package codeware;
 
 public class BreakCamelCase {
+
+    public static String camelCase(String input) {
+
+        StringBuilder output = new StringBuilder();
+
+        for (char c : input.toCharArray()) {
+
+            // change the capital letter to small letter
+            if (c > 64 & c < 91) {
+                c = (char) (c + 32);
+                output.append(" ");
+                output.append(c);
+            }
+
+            // add others letters
+            output.append(c);
+        }
+
+        return output.toString();
+    }
+
 }
