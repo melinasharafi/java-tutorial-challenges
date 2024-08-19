678de9c9a0b707f5c8a5b333b17b3501ea8ec5af
melinasharafi <melinasharafimiab@gmail.com>
2024-08-19 21:22:35 +0330

    fix 'returning null for single char' bug

--- src/codeware/FirstNonRepeatingChar.java
+++ src/codeware/FirstNonRepeatingChar.java
@@ -7,6 +7,10 @@ public class FirstNonRepeatingChar {
         String input = s.toUpperCase();
         int index = -1;
 
+        if (input.length() == 1) {
+            return s;
+        }
+
         for (int i = 0; i < input.length(); i++) {
             for (int j = i + 1; j < input.length(); j++) {
                 if (input.charAt(i) == input.charAt(j)) {
