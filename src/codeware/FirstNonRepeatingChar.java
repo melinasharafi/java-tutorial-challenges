4b6dcdad7012c83e8e93e21fecaf3a4bf82d4958
melinasharafi <melinasharafimiab@gmail.com>
2024-08-19 18:38:56 +0330

    implement firstNonRepeatingLetter(String s) method

--- src/codeware/FirstNonRepeatingChar.java
+++ src/codeware/FirstNonRepeatingChar.java
@@ -1,4 +1,27 @@
 package codeware;
 
 public class FirstNonRepeatingChar {
+
+    public static String firstNonRepeatingLetter(String s){
+
+        String input = s.toUpperCase();
+        int index = -1;
+
+        for (int i = 0; i < input.length(); i++) {
+            for (int j = i + 1; j < input.length(); j++) {
+                if (input.charAt(i) == input.charAt(j)) {
+                    break;
+                }
+                index = i;
+            }
+            if (index != -1) {
+                break;
+            }
+        }
+
+        if (index != -1) {
+            return Character.toString(s.charAt(index));
+        }
+        return "";
+    }
 }
