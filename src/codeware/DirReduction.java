feabfae877655f2266be3e4f375e5cafa7bb23c6
melinasharafi <melinasharafimiab@gmail.com>
2024-08-21 20:15:19 +0330

    optimize dirReduc() using stack

--- src/codeware/DirReduction.java
+++ src/codeware/DirReduction.java
@@ -1,49 +1,42 @@
 package codeware;
 
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class DirReduction {
 
-    private static Map<String, Integer> direction = new HashMap<>();
+    private static Map<String, Integer> directions = new HashMap<>();
 
     static {
-        direction.put("NORTH", 1);
-        direction.put("SOUTH", -1);
-        direction.put("WEST", 2);
-        direction.put("EAST", -2);
+        directions.put("NORTH", 1);
+        directions.put("SOUTH", -1);
+        directions.put("WEST", 2);
+        directions.put("EAST", -2);
     }
 
     public static String[] dirReduc(String[] arr) {
 
-        List<String> finalDirection = new ArrayList<>(Arrays.asList(arr));
+        Stack<String> finalDirection = new Stack<>();
+        int result;
 
-        while (!isDirectlyOpposite(finalDirection)) {
-            for (int i = 0; i < finalDirection.size() - 2; i++) {
-                int iValue = direction.get(finalDirection.get(i));
-                int iPlus1Value = direction.get(finalDirection.get(i + 1));
-
-                if (iValue + iPlus1Value == 0) {
-                    finalDirection.remove(finalDirection.get(i + 1));
-                    finalDirection.remove(finalDirection.get(i));
-                    i--;
+        for (String direction : arr) {
+            if (!finalDirection.isEmpty()) {
+                result = directions.get(finalDirection.peek()) + directions.get(direction);
+                if (result == 0 ) {
+                    finalDirection.pop();
                 }
+                else {
+                    finalDirection.push(direction);
+                }
+            }
+            else {
+                finalDirection.push(direction);
             }
         }
 
         return finalDirection.toArray(new String[0]);
     }
 
-    public static boolean isDirectlyOpposite(List<String> dirctionList) {
 
-        for (int i = 0; i < dirctionList.size() - 1; i++) {
-            if (direction.get(dirctionList.get(i)) + direction.get(dirctionList.get(i + 1)) == 0) {
-                return false;
-            }
-        }
-
-        return true;
-    }
 
     public static void main(String[] args) {
         String[] directions = {"NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"};
