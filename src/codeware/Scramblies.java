56d94ce6c5e9f6c71fab2933096799fbdfc059e8
melinasharafi <melinasharafimiab@gmail.com>
2024-08-26 15:48:35 +0330

    implement scramble() method

--- src/codeware/Scramblies.java
+++ src/codeware/Scramblies.java
@@ -1,4 +1,30 @@
 package codeware;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Scramblies {
+
+
+    public static boolean scramble(String str1, String str2) {
+        // Convert str1 and str2 into List<Character>
+        List<Character> str1ToCharacter = str1.chars().mapToObj(c -> (char) c).collect(Collectors.toList());
+        List<Character> str2ToCharacter = str2.chars().mapToObj(c -> (char) c).collect(Collectors.toList());
+
+        // check if chars at str2ToCharacter are included in str1ToCharacter
+        for (Character c : str1ToCharacter) {
+            if (str2ToCharacter.contains(c)) {
+                str2ToCharacter.remove(c);
+            }
+        }
+
+        // if str2ToCharacter is empty then portion of str1 characters can be rearranged to match str2
+        if (str2ToCharacter.isEmpty()) {
+            return true;
+        }
+
+        return false; // Placeholder return
+    }
+
+
 }
